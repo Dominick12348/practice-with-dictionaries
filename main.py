28c7e2a3fd60ee5169887c78a48d7498726c8564
Dominick12348 <122392919+Dominick12348@users.noreply.github.com>
2023-10-02 13:54:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+#working with apis and dictionaries 
+
+import requests
+import json
+
+response = requests.get("https://randomuser.me/api")
+#print(response.json())
+
+
+title = response.json()['results'][0]['name']['first']
+print(title)
+gender = response.json()['results'][0]['gender']
+print(gender)
+last_name = response.json()['results'][0]['name']['last']
+print(last_name)
+street_name = response.json()['results'][0]['location']['street']['name']
+print(street_name)
+number = response.json()['results'][0]['location']['street']['number']
+city = response.json()['results'][0]['location']['city']
+print(city)
+country = response.json()['results'][0]['location']['country']
+print(country)
+postcode = response.json()['results'][0]['location']['postcode']
+print(postcode)
+email = response.json()['results'][0]['email']
+print(email)
+username = response.json()['results'][0]['login']['username']
+print(username)
